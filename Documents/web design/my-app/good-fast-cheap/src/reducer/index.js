b4df1fabcf4a4cf8b09e7f88592928aaebfd7709
kegilmez <kegilmez@gmail.com>
2022-02-02 22:04:51 +0000

    initial commit

---
+++ Documents/web design/my-app/good-fast-cheap/src/reducer/index.js
@@ -0,0 +1,25 @@
+const initialState = { good: false, cheap: false, fast: false };
+
+const reducer = (state = initialState, action) => {
+  const { label } = action;
+  switch (action.type) {
+    case "TOGGLE_SWITCH":
+      const newState = { ...state, [label]: !state[label] };
+      if (!state[label]) {
+        if (label === "fast" && state.good) {
+          return { ...newState, cheap: false };
+        }
+        if (label === "cheap" && state.fast) {
+          return { ...newState, good: false };
+        }
+        if (label === "good" && state.cheap) {
+          return { ...newState, fast: false };
+        }
+      }
+      break;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
